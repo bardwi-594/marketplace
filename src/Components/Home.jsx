3173c971dd64d2b868e839844868634762f081cb
Bardwi <bardwi@dominiks-iMac.fritz.box>
2021-01-07 14:53:15 +0100

    Added buy functionality

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import Products from "./Products";
+import Buy from "./Buy";
 import data from "../product.json";
 
-export default class Home extends Component {
-    constructor() {
-        super();
-        this.state = {
-          products: data.products,
-        }
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      buyItem: []
+      
+    };
+  }
+ 
+
+  handleBuy= (product) => {
+    const buyItem = this.state.buyItem.slice();
+    let itemExists = false;
+    buyItem.forEach(item=>{
+      if(item) {
+        itemExists = true;
       }
+    });
+    if(!itemExists) {
+      buyItem.push({...product, count: 1})
+    }
+    this.setState({buyItem})
     
-    render() {
+  }
+
+  
+  render() {
     return (
         <div className="app-container">
           <header className="app-header">
@@ -21,8 +41,11 @@ export default class Home extends Component {
           <main>
             <div className="app-content"> 
               <div className="product-container">
-                <Products products={this.state.products}/>
+                <Products products={this.state.products} handleBuy={this.handleBuy}/>
               </div>
+              <div className="sidenav"> 
+                <Buy buyItem={this.state.buyItem} />
+            </div>
             </div>
     
           </main>
@@ -33,4 +56,5 @@ export default class Home extends Component {
       );
     }
  }
+ export default Home;
     
