fc05afa46d6c8e019942a4fe5793bc76cd8e0d47
Bardwi <bardwi@dominiks-iMac.fritz.box>
2021-01-07 11:53:07 +0100

    Product details Screen

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import { withRouter, Link } from "react-router-dom";
 
 export default class Products extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          product: null,
+        };
+      }
     render() {
         return (
             <div className="product-wrapper">
@@ -8,10 +15,15 @@ export default class Products extends Component {
                    {this.props.products.map((product) =>(
                         <li key={product._id}>
                             <div className="products">
-                                <a href={"#" + product._id}>
-                                    <img src = {product.image} alt={product.title}/>
-                                    <p>{product.title}</p>
-                                </a>
+                                <Link to={{pathname: `/product/${product._id}`,
+                                        state:{ product}
+                                        }} key={product._id}> 
+                                        <img src = {product.image} alt={product.title}/>
+                                        <p>
+                                        {product.title}
+                                        </p>
+                                </Link>
+                               
                                 <div className="price">
                                     <div className="prices"><span>€</span>{product.price}</div>
                                     <button className="button buybtn">Buy</button>
