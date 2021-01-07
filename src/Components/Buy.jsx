50d80e1693a7db272cd4ec43dd0ab5928c21071a
Bardwi <bardwi@dominiks-iMac.fritz.box>
2021-01-07 15:56:34 +0100

    Buy wizard/ Customer details form

--- src/Components/Buy.jsx
+++ src/Components/Buy.jsx
@@ -1,6 +1,21 @@
 import React, { Component } from 'react'
+import Modal from "react-modal";
 
 export default class Buy extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+  }
+
+
+  openModal = () => {
+    this.setState({ showModal: true });
+  };
+  closeModal = () => {
+    this.setState({ showModal: false });
+  };
     render() {
         const { buyItem } = this.props;
         return (
@@ -28,8 +43,67 @@ export default class Buy extends Component {
 
                 <div className="total-amt">
                   <div className="final-amt">Total: <span>€</span> </div>
-                  <button className="button-checkout">Buy</button>
+                  <button className="button-checkout" onClick={this.openModal}>Buy</button>
                 </div>
+
+                <Modal isOpen={this.state.showModal}
+                  contentLabel="Enter Personal Details"
+                  className="Modal"
+                  overlayClassName="Overlay"
+                  onRequestClose={this.closeModal}
+                  >
+                  <div className="close-btn">
+                    <button onClick={this.closeModal}>close</button>
+                  </div>
+                  
+                  <div className="checkout-details">
+                    <div>Enter your Details</div>
+                      <form >
+                        <ul className="form-container">
+
+                        <li>
+                            <label>Name</label>
+                             <input
+                              name="name"
+                              type="text"
+                              required
+                             ></input>
+                          </li>
+                          <li>
+                            <label>Email</label>
+                              <input
+                               name="email"
+                               type="email"
+                               required
+                               ></input>
+                          </li>
+                          
+                          <li>
+                             <label>Address</label>
+                              <input
+                              name="address"
+                              type="text"
+                              required
+                              ></input>
+                          </li>
+                          <li>
+                             <label>Mobile</label>
+                              <input
+                              name="mobile"
+                              type="number"
+                              required
+                              ></input>
+                          </li>
+                          <li>
+                              <button className="button-checkout" type="submit">
+                                 Checkout
+                              </button>
+                          </li>
+                      </ul>
+                    </form>
+                  </div>
+                     
+                </Modal>
             </div>
            )
       }
